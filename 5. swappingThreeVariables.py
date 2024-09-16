8a60d905e340d8efc2d9aff0782395ac3f1f9b44
amite <amitendubikashdhusiya@gmail.com>
2024-09-16 20:23:58 +0530

    Deleted the outside ones

--- 5. swappingThreeVariables.py
+++
@@ -1,9 +0,0 @@
-# Write a python program to SWAP three numbers.
-
-a, b, c = 10, 20, 30
-
-print(f"BEFORE SWAPPING: {a, b, c}")
-
-a, b, c = c, b, a
-
-print(f"AFTER SWAPPING: {a, b, c}")
--- 9. calculateSurfaceAreaAndVolumeOfSphere.py
+++
@@ -1,9 +0,0 @@
-PI = 3.14159
-
-radius = float(input("ENTER THE RADIUS: "))
-
-area = 4 * PI * (radius ** 2)
-vol = 4 / 3 * PI * radius ** 3
-
-print("THE SURFACE AREA AND THE VOLUME OF THE SPHERE IS:: ")
-print(f"VOLUME: {vol}, \nSURFACE AREA: {area}")
